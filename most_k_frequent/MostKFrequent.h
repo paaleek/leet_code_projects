8b4546453b15dda98cb36a416fe2812375f38ad0
paaleek <114878175+paaleek@users.noreply.github.com>
2023-08-08 16:53:22 +0200

    MostKFrequent example done, even top 98% note: explore using heap to see if it has a time benefit or not

--- most_k_frequent/MostKFrequent.h
+++ most_k_frequent/MostKFrequent.h
@@ -6,12 +6,39 @@
 #define LEET_CODE_PRIJECTS_MOSTKFREQUENT_H
 #include <vector>
 #include <unordered_map>
+#include <queue>
+#include <algorithm>
 using namespace std;
 
 class MostKFrequent {
 public:
+    static bool cmp(pair<int, int>& a, pair<int, int>& b) {
+        return a.second > b.second;
+    }
+
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<>
+        unordered_map<int, int> counts;
+
+        for (int number: nums) {
+            counts[number]++;
+        }
+
+        vector<pair<int, int>> pairs;
+
+        for (auto& iter: counts) {
+            pairs.push_back(iter);
+        }
+        counts.erase(counts.begin()); //little benefit in memmory, and runs slightly slower
+
+        sort(pairs.begin(), pairs.end(), cmp);
+
+        vector<int> result;
+
+        for (int i = 0; i < k; i++) {
+            result.push_back(pairs.at(i).first);
+        }
+
+        return result;
     }
 };
 
